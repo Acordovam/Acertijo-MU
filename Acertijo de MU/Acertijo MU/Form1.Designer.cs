896b52695e4744567b96e20290a4308792360835
Alejandro Cordova <33092112+Acordovam@users.noreply.github.com>
2019-01-22 20:14:50 -0600

    Unica subida
    Subida de archivos única. Hecho en Visual Studio 2017.

---
+++ Acertijo de MU/Acertijo MU/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Acertijo_MU
+{
+    partial class form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.BackColor = System.Drawing.Color.SkyBlue;
+            this.list.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.list.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list.Location = new System.Drawing.Point(13, 13);
+            this.list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(818, 529);
+            this.list.TabIndex = 1;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.GhostWhite;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(411, 586);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbuscar.Location = new System.Drawing.Point(191, 589);
+            this.txtbuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(212, 29);
+            this.txtbuscar.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.GhostWhite;
+            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSeaGreen;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Papyrus", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(700, 598);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(846, 28);
+            this.panel1.TabIndex = 5;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Acertijo_MU.Properties.Resources._373654;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(845, 654);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.list);
+            this.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "form";
+            this.Opacity = 0.9D;
+            this.Text = "Alejandro Cordova - Acertijo de MU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView list;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Acertijo de MU/Acertijo MU/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+namespace Acertijo_MU
+{
+    
+    public partial class form : Form
+    {
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        String[] arrcadenas = new String[100000]; 
+        int mindice = 0;
+        public form()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        public string Regla1(String cadena)
+        {
+            cadena = cadena + 'U';
+            return cadena;
+        }
+        public string Regla2(String cadena)
+        {
+            cadena = cadena + cadena.Substring(1); 
+            return cadena;
+        } 
+        public string Regla3(String cadena)
+        {
+            cadena = cadena.Replace("III", "U");
+            return cadena;
+        }
+        public string Regla4(String cadena)
+        {
+            cadena = cadena.Replace("UU", ""); 
+            return cadena;
+        }
+        
+        void buscarmu(String cadena, String cadenasal)
+        {
+           
+            if (cadenasal != cadena)
+            {
+              
+            }
+            if(cadenasal != "MU"){
+                mindice++;
+                arrcadenas[mindice] = cadenasal;
+
+            }
+            else
+            {
+                MessageBox.Show("Encontrado");
+            }
+        }
+       public void generarMU(String cadena)
+        {
+            String cad;
+            int longitud;
+            String cadenasal1, cadenasal2, cadenasal3, cadenasal4;
+            longitud = cadena.Length;
+            cad = cadena.Substring(longitud-1);
+            
+            if(cad == "I")
+            {
+                cadenasal1 = Regla1(cadena);
+                buscarmu(cadena, cadenasal1);   
+            }
+            cadenasal2 = Regla2(cadena);  
+            buscarmu(cadena, cadenasal2); 
+            if (cadena.IndexOf("III") == 1)
+            {
+                cadenasal3 = Regla3(cadena);      
+                buscarmu(cadena, cadenasal3);    
+            }
+            if (cadena.IndexOf("UU") == 1)
+            {
+                cadenasal4 = Regla4(cadena);
+                buscarmu(cadena, cadenasal4);   
+            }
+            
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+        
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            arrcadenas[0] = "MI"; 
+            list.Items.Add("MI");
+            int i = 0;
+            while (i < 50000) 
+            {
+                generarMU(arrcadenas[i]);
+                if (arrcadenas[i] == "M" || arrcadenas[i].Contains("III") || arrcadenas[i].Contains("UU"))
+                {
+                }
+                else
+                {
+                    list.Items.Add(arrcadenas[i]);
+
+                }
+
+                i++;
+            }
+
+
+
+
+
+            String buscar = txtbuscar.Text; 
+            ListViewItem item1 = list.FindItemWithText(buscar);
+            if (item1 != null && buscar != "MU" && buscar != "" && buscar != " ")
+            {
+                MessageBox.Show("Se ha encontrado el Teorema '"+buscar+"' entre las iteraciones");
+            }
+            else
+            {
+                MessageBox.Show("NO se ha encontrado el Teorema '" + buscar + "' entre las iteraciones, por lo que no es un teorema");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
